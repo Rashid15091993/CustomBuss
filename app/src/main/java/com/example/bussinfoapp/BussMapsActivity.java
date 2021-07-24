8ceeec9f0e3c886e5766c8cdfb8eda873122d081
rashi <rashid058@mail.ru>
2021-07-24 22:51:37 +0500

    24.07.2021

--- app/src/main/java/com/example/bussinfoapp/BussMapsActivity.java
+++ app/src/main/java/com/example/bussinfoapp/BussMapsActivity.java
@@ -15,9 +15,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -44,36 +48,34 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.List;
-
+//Приложение для клиентов
 public class BussMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private ActivityBussMapsBinding binding;
     private GoogleMap mMap;
     private Geocoder geocoder;
-    public static final int PERMISSIONS_FINE_LOCATION = 99;
+
     private int ACCESS_LOCATION_REQUEST_CODE = 10001;
+    private int radius = 1;
+    private Boolean driverFound  = false;
+    private String driverFoundID;
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
 
-    private DatabaseReference assignedCustomRef, AssignedCustomPositionRef;
-    private String driverID, customID = "";
+    Marker userLocationMarker, driverBussMarket;
+    Circle userLocationAccuracyCircle;
 
-    private ValueEventListener AssignedCustomPositionListener;
-    Marker PickUpMarker;
-
-
+    private String customerID;
+    private DatabaseReference DriversAvailableRef;
 
     private FirebaseAuth mAuth;
-    private FirebaseUser currentUser;
-    private DatabaseReference CustomerDatabaseRef;
+    private Button callBussBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
-        }
 
         binding = ActivityBussMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -83,7 +85,12 @@ public class BussMapsActivity extends FragmentActivity implements OnMapReadyCall
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        mAuth = FirebaseAuth.getInstance();
+        DriversAvailableRef = FirebaseDatabase.getInstance().getReference().child("Driver Available");
 
+        callBussBtn = (Button) findViewById(R.id.callBuss);
+
+        geocoder = new Geocoder(this);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         locationRequest = LocationRequest.create();
@@ -91,83 +98,151 @@ public class BussMapsActivity extends FragmentActivity implements OnMapReadyCall
         locationRequest.setFastestInterval(500);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
+        callBussBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zoomToUserLocation();
+
+            }
+        });
+    }
+    //Функция определения местоположения автобуса
+    private void getNearbyDriversBuss(LatLng location) {
+        GeoFire geoFire = new GeoFire(DriversAvailableRef);
+        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(location.latitude,location.longitude), radius);
+        geoQuery.removeAllListeners();
+
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+
+                if (!driverFound) {
+                    driverFound = true;
+                    driverFoundID = key;
+
+                }
+                else {
+                    if (driverBussMarket != null) {
+                        driverBussMarket.remove();
+                    }
+                    LatLng latLng = new LatLng(location.latitude, location.longitude);
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng);
+                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.bus_icon));
+                    driverBussMarket = mMap.addMarker(markerOptions);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+
+                }
+
+
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                if (!driverFound) {
+                    radius = radius + 1;
+                    getNearbyDriversBuss(location);
+                }
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+
+            }
+        });
 
     }
+
+
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        zoomToUserLocation();
 
-            return;
+    }
+
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+
+            if (mMap != null) {
+                setUserLocationMarker(locationResult.getLastLocation());
+
+
+            }
         }
-        mMap.setMyLocationEnabled(true);
-        AssignedBussDriversPosition();
+    };
+
+    private void setUserLocationMarker(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        if (userLocationMarker == null) {
+            //Create a new market
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.user));
+            markerOptions.rotation(location.getBearing());
+            userLocationMarker = mMap.addMarker(markerOptions);
+
+        } else {
+            userLocationMarker.setPosition(latLng);
+            userLocationMarker.setRotation(location.getBearing());
+
+        }
+
 
     }
 
-    private void getAssignedCustomRequest() {
-        assignedCustomRef = FirebaseDatabase.getInstance().getReference().child("Users")
-                .child(driverID);
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
 
-        assignedCustomRef.addValueEventListener(new ValueEventListener() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startLocationUpdates();
+        } else {
+
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void zoomToUserLocation() {
+        Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()) {
-                    customID = snapshot.getValue().toString();
+            public void onSuccess(Location location) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
 
-                    AssignedBussDriversPosition();
-
-                }
-                else {
-                    customID = "";
-
-                    if(PickUpMarker != null) {
-                        PickUpMarker.remove();
-                    }
-                    if(AssignedCustomPositionListener != null) {
-                        AssignedCustomPositionRef.removeEventListener(AssignedCustomPositionListener);
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                getNearbyDriversBuss(latLng);
 
             }
         });
     }
-
-    private void AssignedBussDriversPosition() {
-        AssignedCustomPositionRef = FirebaseDatabase.getInstance().getReference().child("Custom Requests")
-                .child(customID).child("l");
-
-        AssignedCustomPositionListener = AssignedCustomPositionRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    List<Object> customerPositionMap = (List<Object>) snapshot.getValue();
-                    double LocationLat = LocationLat = Double.parseDouble(customerPositionMap.get(0).toString());
-                    ;
-                    double LocationLng = LocationLng = Double.parseDouble(customerPositionMap.get(1).toString());
-
-                    LatLng DriverLatLng = new LatLng(LocationLat, LocationLng);
-
-                    PickUpMarker = mMap.addMarker(new MarkerOptions().position(DriverLatLng)
-                            .title("Забрать клиента тут").icon(BitmapDescriptorFactory.fromResource(R.drawable.user)));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(DriverLatLng));
-                    mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-    }
-
-
-
 }
--- app/src/main/java/com/example/bussinfoapp/MainActivity.java
+++ app/src/main/java/com/example/bussinfoapp/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.bussinfoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int PERMISSIONS_FINE_LOCATION = 99;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//Выходит диалоговое окно о разрешение на предоставление геолокации
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+        }
 
         Thread thread = new Thread()
         {
